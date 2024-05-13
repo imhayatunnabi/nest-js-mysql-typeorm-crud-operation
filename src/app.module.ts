35081ccf5d2a8761a00a36be774b8176621fdfd4
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2024-05-13 18:31:46 +0600

    module, controller and service has been created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/User';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { User } from './entities/User';
       synchronize: true,
       entities:[User]
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
