b473ff98bdf8dcaaa978270b52e773e88aebdf12
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2024-05-13 18:19:47 +0600

    TypeOrm configuration has been added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'P@$$Wor8',
+      database: 'nestjsmysqlcrud',
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
